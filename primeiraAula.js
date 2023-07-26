f3ac2903ae2551acd531fdfce3e2e2c1711daa4f
MatheusFreitasG <131834576+MatheusFreitasG@users.noreply.github.com>
2023-07-26 13:34:36 -0300

    Primeiro upload dos exercícios da aula

---
+++ primeiraAula.js
@@ -0,0 +1,78 @@
+//minha idade Exercicio 1
+console.log('tenho 16 anos')
+
+//semanas que programei nas férias Exercicio 2
+console.log('4 semanas')
+//estudei c++ para melhorar minha logica de programação
+
+/*
+Exercicio 3
+o console.log() serve para imprimir um numero na tela
+o console é um objeto
+let é para criar variaveis
+*/
+
+//definir variaveis Exercicio 4
+
+//variavel de number
+let variavel1 = 16
+//variavel string
+let variavel2 = 'Meu nome é Matheus mas me chamam de Freitas'
+//variavel buleana
+let variavel3 = true
+//variavel de objeto
+let variavel4 = console.log(16+4)
+// variavel de simbulo
+let variavel5 = Symbol('Freitinhas')
+
+//conta Exercicio 5
+
+console.log(16+3.5)
+
+// conta Exercicio 6
+
+dataAtual = new Date(2023)
+dataLua = new Date(1969)
+console.log(dataAtual - dataLua)
+
+// Exercicio 7
+
+let divisao = 65/240
+console.log(divisao)
+
+// Exercicio 8
+
+let multiply = 0.2708 * 100
+console.log(multiply)
+
+//Exercicio 9
+
+console.log('Hello' + 'World')
+
+//Exercicio 10
+
+console.log('Hello ' + 'World')
+
+//Exercicio 11
+
+console.log('Teaching the world how to code'.length)
+
+//Exercicio 12
+
+console.log('Kaian Novais'.toUpperCase())
+
+//Exercicio 13
+
+console.log(' Professor Kaian Novais '.trim())
+
+//Exercicio 14
+
+console.log(Math.random(1) * 100)
+
+//Exercicio 15
+
+console.log(Math.ceil(43.8))
+
+//Exercicio 16
+
+console.log(Number.isInteger(2017))
